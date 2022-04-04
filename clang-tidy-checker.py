909f75cabbf8c18aa23a55fafc7818e138484073
HabKaffee <nikita.naumov@intel.com>
2022-04-04 13:58:39 +0300

    raw version of clang-tidy checker implemented

--- clang-tidy-checker.py
+++ clang-tidy-checker.py
@@ -1,26 +1,33 @@
 import argparse
 import os
-import shutil
+import yaml
 import sys
 
 def do_check():
-    #os.system('clear')
+    debug = True # debug purposes
     current_dir = os.getcwd()
-    llvm_dir = current_dir # for debug purposes. When script would be implemented - llvm_dir and current_dir would be different
+    llvm_dir = current_dir # When script would be implemented - llvm_dir and current_dir would be different
     sycl_dir = llvm_dir + os.sep + 'sycl' + os.sep
+    #include_dir = f'{sycl_dir}{os.sep}include{os.sep}CL{os.sep}sycl{os.sep}'
+    src_dir = f'{sycl_dir}source{os.sep}'
     os.chdir(sycl_dir)
-    shutil.copyfile(llvm_dir+os.sep+'.clang-tidy', llvm_dir+os.sep+'.clang-tidy.txt')
     current_dir = os.getcwd()
+    extensions = ['.h', '.hpp', '.c', '.cc', '.cpp']
+    files_in_src_folder = []
     #print (current_dir)
-    tidy_options = '{'
-    with open(llvm_dir + os.sep + '.clang-tidy.txt', 'r') as f:
-        for _ in f:
-            tidy_options += f.readline()
-    tidy_options += '}'
-    print(tidy_options)
-    
-    os.remove(llvm_dir+os.sep+'.clang-tidy.txt')
-
+    tidy_options_dict = {}
+    with open (llvm_dir + os.sep + '.clang-tidy', 'r') as f:
+        tidy_options_dict = yaml.safe_load(f)
+    #print(f'Tidy dict {tidy_options_dict}')
+    for root, directory, file in os.walk(src_dir):
+        for _file in file:
+            for ext in extensions:
+                if _file.endswith(ext):
+                    files_in_src_folder.append(os.path.join(root, _file))
+    #print(files_in_src_folder)
+    for file_to_check in files_in_src_folder:
+        os.system(f'clang-tidy -header-filter=.* -config="{tidy_options_dict}" -p {llvm_dir}/build {file_to_check}')
+        if debug: break
 
 def main() :
     
